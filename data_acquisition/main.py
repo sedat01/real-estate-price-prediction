5fad211546da70cc30e5b23820ec3a27550f5338
sedat01 <sedat9404@gmail.com>
2022-11-04 14:14:14 +0100

    edited readme

--- data_acquisition/main.py
+++ data_acquisition/main.py
@@ -18,6 +18,3 @@ for province in range(len(provinces)):
     grand_json, grand_tables = property_explorer_requests.explorer(province=provinces[province],province1=provinces1[province])
     print(len(grand_json))
 
-
-df1 = pd.DataFrame.from_dict(grand_json,orient="index")
-df2 = pd.DataFrame.from_dict
